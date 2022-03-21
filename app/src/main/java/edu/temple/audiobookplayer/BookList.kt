e8231c748b2692eedab8aeabf3050be4df376320
mailoalex <tun29878@temple.edu>
2022-03-21 11:48:33 -0400

    created and finished BookList class.

---
+++ app/src/main/java/edu/temple/audiobookplayer/BookList.kt
@@ -0,0 +1,31 @@
+package edu.temple.audiobookplayer
+
+class BookList {
+    var books = ArrayList<Book>()
+
+    // add book to book list
+    fun add(_book: Book ){
+        books.add(_book)
+    }
+
+    // remove book from book list
+    fun remove(_book: Book){
+        books.remove(_book)
+    }
+
+    // get book with index
+    fun get(_index: Int ) : Book{
+        return books.get(_index)
+    }
+
+    // get amount of books in BookList
+    fun size() : Int {
+        return books.size
+    }
+
+    
+
+
+
+
+}
