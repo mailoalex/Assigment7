451e80646be74cd6bf15638e141f75176cd0fa85
mailoalex <tun29878@temple.edu>
2022-03-21 11:54:28 -0400

    added 10 books to a new booklist

--- app/src/main/java/edu/temple/audiobookplayer/BookList.kt
+++ app/src/main/java/edu/temple/audiobookplayer/BookList.kt
@@ -23,7 +23,7 @@ class BookList {
         return books.size
     }
 
-    
+
 
 
 
--- app/src/main/java/edu/temple/audiobookplayer/MainActivity.kt
+++ app/src/main/java/edu/temple/audiobookplayer/MainActivity.kt
@@ -7,5 +7,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val list_books = BookList()
+        list_books.add(Book("Sublime","Yvor Wasbey"))
+        list_books.add(Book("Smoke","Tanny Petteford"))
+        list_books.add(Book(" The Old Gun","Yvor Wasbey"))
+        list_books.add(Book("Meatballs Part II","Gisela Urquhart"))
+        list_books.add(Book("Phantasm II","Trudi Rochell"))
+        list_books.add(Book("Fudoh","Lynnette McBay"))
+        list_books.add(Book("Sanctum","Lindy Orcas"))
+        list_books.add(Book("Just a Sigh","Jesselyn Vallintine"))
+        list_books.add(Book("Miehen tie","Griffin McCree"))
+
+
+
     }
 }
